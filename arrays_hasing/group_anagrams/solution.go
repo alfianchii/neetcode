adbb553a5ce7d50d6c034df5e4d4ada8e1e7fd4d
Alfian Taka <pianaee379@gmail.com>
2026-02-20 07:57:57 +0700

    arrays: brute force (sorting ver.) Group Anagrams

--- arrays_hasing/group_anagrams/solution.go
+++ arrays_hasing/group_anagrams/solution.go
@@ -1,47 +1,27 @@
 package main
 
 import (
-	"slices"
+	"sort"
 )
 
-func isAnagram(s string, t string) bool {
-	if len(s) != len(t) {
-		return false
-	}
-
-	freq := make(map[rune]int)
-
-	for _, ch := range s {
-		freq[ch]++
-	}
-
-	for _, ch := range t {
-		freq[ch]--
-		if freq[ch] < 0 {
-			return false
-		}
-	}
-
-	return true
-}
-
 func GroupAnagrams(strs []string) [][]string {
-	result := [][]string{}
-	for i := 0; i < len(strs); i++ { // 1
-		// ALL STRS [act pots tops cat stop hat]
-		// STRS [act, pots, stop, hat]
-		s := strs[i] // pots
-		// RESULT [[act cat], [pots, tops]]
-		result = slices.Insert(result, len(result), []string{s})
-		for j := i + 1; j < len(strs); j++ { // 1 + 1 = 2
-			t := strs[j] // tops
-			if ok := isAnagram(s, t); ok {
-				result[i] = slices.Insert(result[i], len(result[i]), t)
-				strs = slices.Delete(strs, j, j+1)
-				j--
-			}
-		}
+	groups := make(map[string][]string)
+
+	for _, s := range strs {
+		chars := []byte(s)
+
+		sort.Slice(chars, func(i, j int) bool {
+			return chars[i] < chars[j]
+		})
+
+		key := string(chars)
+		groups[key] = append(groups[key], s)
 	}
 
-	return result
+	res := make([][]string, 0, len(groups))
+	for _, group := range groups {
+		res = append(res, group)
+	}
+
+	return res
 }
