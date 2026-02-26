741136fdaa18f3d7768d2c228f3d75770fd03f35
Alfian Taka <pianaee379@gmail.com>
2026-02-27 06:22:32 +0700

    feat: hash set

--- arrays_hasing/valid_sudoku/solution.go
+++ arrays_hasing/valid_sudoku/solution.go
@@ -1,53 +1,32 @@
 package main
 
 func IsValidSudoku(board [][]byte) bool {
-	currRow := make(map[byte]int)
-	currColumn := make(map[byte]int)
+	rows := make([]map[byte]bool, 9)
+	cols := make([]map[byte]bool, 9)
+	boxes := make([]map[byte]bool, 9)
 
-	for _, rows := range board {
-		for _, row := range rows {
-			_, exists := currRow[row]
-			if exists && row != '.' {
-				return false
-			}
-			currRow[row]++
-		}
-
-		currRow = make(map[byte]int)
+	for i := range rows {
+		rows[i] = make(map[byte]bool)
+		cols[i] = make(map[byte]bool)
+		boxes[i] = make(map[byte]bool)
 	}
 
-	for i, rows := range board {
-		for j := range rows {
-			column := board[j][i]
-			_, exists := currColumn[column]
-			if exists && column != '.' {
+	for r := range 9 {
+		for c := range 9 {
+			val := board[r][c]
+
+			if val == '.' {
+				continue
+			}
+
+			boxIdx := (r/3)*3 + (c / 3)
+			if rows[r][val] || cols[c][val] || boxes[boxIdx][val] {
 				return false
 			}
-			currColumn[column]++
-		}
 
-		currColumn = make(map[byte]int)
-	}
-
-	countColumn := 0
-	for i := 0; i < len(board); i++ {
-		rows := board[i]
-
-		for j := countColumn; j < countColumn+3; j++ {
-			row := rows[j]
-			_, exists := currRow[row]
-			if exists && row != '.' {
-				return false
-			}
-			currRow[row]++
-		}
-
-		if i%3 == 2 {
-			currRow = make(map[byte]int)
-			if countColumn < 6 {
-				i = -1
-				countColumn += 3
-			}
+			rows[r][byte(val)] = true
+			cols[c][byte(val)] = true
+			boxes[boxIdx][byte(val)] = true
 		}
 	}
 
