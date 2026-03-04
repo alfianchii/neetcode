b8851a9ec9968b9c0840ff4ff8080f78c7649c16
Alfian Taka <pianaee379@gmail.com>
2026-03-04 15:08:50 +0700

    feat: array solution

---
+++ arrays_hasing/design_hashmap/solution.go
@@ -0,0 +1,25 @@
+package main
+
+type MyHashMap struct {
+	bucket []int
+}
+
+func Constructor() MyHashMap {
+	bucket := make([]int, 1000001)
+	for i := range bucket {
+		bucket[i] = -1
+	}
+	return MyHashMap{bucket: bucket}
+}
+
+func (this *MyHashMap) Put(key int, value int) {
+	this.bucket[key] = value
+}
+
+func (this *MyHashMap) Get(key int) int {
+	return this.bucket[key]
+}
+
+func (this *MyHashMap) Remove(key int) {
+	this.bucket[key] = -1
+}
